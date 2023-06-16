116ebd857348bf448535fc57ebfbbbe4d766e344
bautancredi <bautistatancredi@hotmail.com>
2023-06-16 12:24:30 -0300

    Update version with changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,61 @@
 module.exports = {
-  "root": true,
-  "extends": [
-    "standard",
-    "prettier",
-    "eslint:recommended"
-  ],
-  "plugins": [
-    "prettier"
-  ],
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "ecmaVersion": 2016,
-    "sourceType": "module"
-  },
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "node": true,
-    "es6": true,
-    "mocha": true
-  },
-  "rules": {
-    "no-debugger": "off",
-    "no-console": "off",
-    "prettier/prettier": [
-      "error",
-      {
-        "semi": true,
-        "bracketSpacing": false,
-        "trailingComma": "es5",
-        "printWidth": 120,
-        "endOfLine": "auto"
+    "env": {
+      "browser": false,
+      "es2021": true,
+      "node": true
+    },
+    "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:tailwindcss/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking"
+    ],
+    "plugins": ["react", "import", "@typescript-eslint"],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+      "ecmaFeatures": {
+        "jsx": true
+      },
+      "project": true
+    },
+    "root": true,
+    "settings": {
+      "react": {
+        "version": "detect"
       }
-    ]
+    },
+    "rules": {
+      // "no-console": "warn",
+      "react/react-in-jsx-scope": "off",
+      "import/order": [
+        "error",
+        {
+          "groups": ["builtin", "external", "parent", "sibling", "index"],
+          "newlines-between": "always"
+        }
+      ],
+      "react/self-closing-comp": "warn",
+      "react/jsx-sort-props": [
+        "warn",
+        {
+          "callbacksLast": true,
+          "shorthandFirst": true,
+          "noSortAlphabetically": false,
+          "reservedFirst": true
+        }
+      ],
+      "padding-line-between-statements": [
+        "warn",
+        { "blankLine": "always", "prev": "*", "next": "return" },
+        { "blankLine": "always", "prev": ["const", "let", "var"], "next": "*" },
+        {
+          "blankLine": "any",
+          "prev": ["const", "let", "var"],
+          "next": ["const", "let", "var"]
+        }
+      ]
+    }
   }
-}
+  
